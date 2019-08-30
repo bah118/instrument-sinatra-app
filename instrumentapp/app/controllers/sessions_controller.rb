7a70056a24865ab09c38d52d67da69825735af5b
Beth Ann <herbivore137@gmail.com>
2019-08-30 15:03:21 -0400

    add signup validation

--- instrumentapp/app/controllers/sessions_controller.rb
+++ instrumentapp/app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
         erb :'sessions/signup'
     end
 
+    post '/signup' do
+        user = User.new(params)
+        if user.save
+
+        else
+            @errors = user.errors.full_messages
+            erb :'sessions/signup'
+        end
+    end
+
     get '/login' do
         erb :'sessions/login'
     end
