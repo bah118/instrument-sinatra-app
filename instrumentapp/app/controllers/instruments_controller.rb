1061253ea0d6ea392c30697cb852852789690ef5
Beth Ann <herbivore137@gmail.com>
2019-08-30 10:23:38 -0400

    add new instrument form

--- instrumentapp/app/controllers/instruments_controller.rb
+++ instrumentapp/app/controllers/instruments_controller.rb
@@ -8,14 +8,16 @@ class InstrumentsController < ApplicationController
     get '/instruments/new' do
         erb :'instruments/new'
     end
-
-    
     
     get '/instruments/:id' do
         @instrument = Instrument.find_by_id(params[:id])
         erb :'instruments/show'
     end
     
-    
+    post '/instruments' do
+        # @instruments = Instrument.all
+        @instrument = Instrument.create(name: params[:instrument_name])
+        redirect '/instruments'
+    end
 
 end
